d11aae22a826ef747f43a424ee5c7999d37d8957
Alliphy <ruebaker29@gmail.com>
2024-07-12 18:23:52 -0600

    queries tests passing

--- src/model.js
+++ src/model.js
@@ -1,8 +1,8 @@
-import { DataTypes, Model } from 'sequelize';
-import util from 'util';
-import connectToDB from './db.js';
+import { DataTypes, Model } from "sequelize";
+import util from "util";
+import connectToDB from "./db.js";
 
-const db = await connectToDB('postgresql:///animals');
+const db = await connectToDB("postgresql:///animals");
 
 export class Human extends Model {
   [util.inspect.custom]() {
@@ -10,11 +10,32 @@ export class Human extends Model {
   }
 
   getFullName() {
-    // TODO: Implement this method
+    return this.fname + " " + this.lname;
   }
 }
 
-// TODO: Human.init()
+Human.init(
+  {
+    humanId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      notNull: true,
+    },
+    fname: {
+      type: DataTypes.CHAR,
+      notNull: true,
+    },
+    lname: {
+      type: DataTypes.CHAR,
+      notNull: true,
+    },
+    email: {
+      type: DataTypes.CHAR,
+      notNull: true,
+    },
+  },
+  { sequelize: db, underscored: true, tableName: "humans" }
+);
 
 export class Animal extends Model {
   [util.inspect.custom]() {
@@ -22,8 +43,33 @@ export class Animal extends Model {
   }
 }
 
-// TODO: Animal.init()
+Animal.init(
+  {
+    animalId: {
+      type: DataTypes.INTEGER,
+      primaryKey: true,
+      notNull: true,
+    },
+    name: {
+      type: DataTypes.CHAR,
+      notNull: true,
+    },
+    species: {
+      type: DataTypes.CHAR,
+      notNull: true,
+    },
+    birthYear: {
+      type: DataTypes.INTEGER,
+    },
+  },
+  {
+    sequelize: db,
+    underscored: true,
+    tableName: "animals",
+  }
+);
 
-// TODO: Define Relationship
+Animal.belongsTo(Human, { foreignKey: "humanId" });
+Human.hasMany(Animal, { foreignKey: "humanId" });
 
 export default db;
